92372735db95f116bc374a720b84eb8ea065fa8e
BrianAnco <ancobrian6@gmail.com>
2025-05-08 16:09:22 -0500

    Edit TDR

--- TRElectrosur/Models/TDR.cs
+++ TRElectrosur/Models/TDR.cs
@@ -1,6 +1,4 @@
 ﻿// Models/TDR.cs
-using System.Text.Json.Serialization;
-
 namespace TRElectrosur.Models
 {
     public class TDR
@@ -14,20 +12,10 @@ namespace TRElectrosur.Models
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public int? UpdatedByUserID { get; set; }
-
-        // Propiedades para la vista (elegir una de las dos implementaciones)
-
-        // Si la API devuelve TypeName y StateName
-        public string TypeName { get; set; }
-        public string StateName { get; set; }
-
-        // O usar ambos nombres de propiedades con JsonPropertyName si es necesario
-        [JsonPropertyName("typeName")]
+        
+        // Propiedades de navegación
         public string TDRTypeName { get; set; }
-
-        [JsonPropertyName("stateName")]
         public string CurrentStateName { get; set; }
-
         public string CreatedByUserName { get; set; }
         public string UpdatedByUserName { get; set; }
         public string AreaName { get; set; }
